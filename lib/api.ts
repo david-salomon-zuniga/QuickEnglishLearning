5f41a2d754c01063259a844596d1a96db58b713f
david-salomon-zuniga <salomon1142608010@gmail.com>
2026-06-19 19:06:43 -0600

    feat: forgot to update API_BASE property

--- lib/api.ts
+++ lib/api.ts
@@ -1,8 +1,7 @@
 // lib/api.ts
-//export const API_BASE = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:7860";
-
+export const API_BASE = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:7860";
 // Hardcode it temporarily to verify the connection
-export const API_BASE = "http://localhost:7860";
+//export const API_BASE = "http://localhost:7860";
 
 if (!API_BASE) {
     throw new Error("NEXT_PUBLIC_BACKEND_URL is not defined in the environment variables!");
